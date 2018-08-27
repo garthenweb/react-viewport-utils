7c6bbd7b588da60c21fc2c5c3d4c12c7352bd43b
Jannick Garthen <jannick.garthen@gmail.com>
2018-08-27 22:00:27 +0200

    chore(*): fix prettier

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
 export { default as ViewportProvider } from './ViewportProvider';
 export { default as connectViewportScroll } from './connectViewportScroll';
-export { default as ObserveBoundingClientRect } from './ObserveBoundingClientRect';
+export {
+  default as ObserveBoundingClientRect,
+} from './ObserveBoundingClientRect';
