59231f95466e31368be6a394ca4efc1ca584ddf2
Jannick Garthen <jannick.garthen@aboutyou.de>
2018-09-10 14:40:51 +0200

    fix(ObserveViewport): fix server side rendering
    Using default it was working on the client, using non default it was working
    on the server. No method worked on both. This should fix it for now.

--- lib/ObserveViewport.tsx
+++ lib/ObserveViewport.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-const memoize = require('memoize-one').default;
+const memoizeOne = require('memoize-one');
+const memoize =
+  typeof memoizeOne === 'function' ? memoizeOne : memoizeOne.default;
 const raf = require('raf');
 
 import { shallowEqualScroll, shallowEqualDimensions } from './utils';
