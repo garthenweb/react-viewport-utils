garthenweb/react-viewport-utils

commit 8325ce6ab00bad68b85d9e597ebd301a8779a4ca

feat(useRect): allow passing deps argument to useRect and useRectEffect to force updates
Closes #18

lib/hooks.ts
@@ -153,11 +153,26 @@ export const useDimensions = (options: IOptions = {}): IDimensions => {
   return dimensions;
 };
 
-export const useRectEffect = (
+export function useRectEffect(
   effect: (rect: IRect | null) => void,
   ref: RefObject<HTMLElement>,
-  options?: IFullOptions,
-) => {
+  deps?: DependencyList,
+): void;
+
+export function useRectEffect(
+  effect: (rect: IRect | null) => void,
+  ref: RefObject<HTMLElement>,
+  options: IFullOptions,
+  deps?: DependencyList,
+): void;
+
+export function useRectEffect(
+  effect: (rect: IRect | null) => void,
+  ref: RefObject<HTMLElement>,
+  third?: any,
+  fourth?: any,
+) {
+  const { options, deps } = sortArgs(third, fourth);
   useViewportEffect(
     (_, snapshot) => effect(snapshot),
     {
@@ -165,20 +180,33 @@ export const useRectEffect = (
       recalculateLayoutBeforeUpdate: () =>
         ref.current ? ref.current.getBoundingClientRect() : null,
     },
-    [ref.current],
+    [ref.current, ...deps],
   );
-};
+}
 
-export const useRect = (
+export function useRect(
   ref: RefObject<HTMLElement>,
-  options?: IFullOptions,
-): IRect | null => {
+  deps?: DependencyList,
+): void;
+
+export function useRect(
+  ref: RefObject<HTMLElement>,
+  options: IFullOptions,
+  deps?: DependencyList,
+): void;
+
+export function useRect(
+  ref: RefObject<HTMLElement>,
+  second: any,
+  third?: any,
+): IRect | null {
+  const { options, deps } = sortArgs(second, third);
   return useLayoutSnapshot(
     () => (ref.current ? ref.current.getBoundingClientRect() : null),
     options,
-    [ref.current],
+    [ref.current, ...deps],
   );
-};
+}
 
 export function useLayoutSnapshot<T = unknown>(
   recalculateLayoutBeforeUpdate: (viewport: IViewport) => T,

commit d98c87f7bd70dd7d75fc0adeb127fa0ec4a30f36

fix(useRect): set the correct typescript return types
This regression was introduced in 8325ce6ab00bad68b85d9e597ebd301a8779a4ca

lib/hooks.ts
@@ -187,13 +187,13 @@ export function useRectEffect(
 export function useRect(
   ref: RefObject<HTMLElement>,
   deps?: DependencyList,
-): void;
+): Rect | null;
 
 export function useRect(
   ref: RefObject<HTMLElement>,
   options: FullOptions,
   deps?: DependencyList,
-): void;
+): Rect | null;
 
 export function useRect(
   ref: RefObject<HTMLElement>,